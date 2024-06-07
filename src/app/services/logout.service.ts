305f7b2e50c26e6b800de1068bfb3cb3ed0167ad
Felix <felix.achter.it@gmail.com>
2024-06-07 10:58:47 +0200

    refactoring

--- src/app/services/logout.service.ts
+++ src/app/services/logout.service.ts
@@ -1,6 +1,7 @@
 import {inject, Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
 import {MenuService} from "./menu.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: "root"
@@ -8,10 +9,12 @@ import {MenuService} from "./menu.service";
 export class LogoutService {
   authService = inject(AuthService)
   menuService = inject(MenuService)
+  router = inject(Router)
 
   logout() {
     this.authService.currentUser.set(null);
     localStorage.removeItem("token")
     this.menuService.setMenu();
+    this.router.navigate(["/login"]).then();
   }
 }

1f623daf18414f7d14433bb428521a68fc8daeab
Felix <felix.achter.it@gmail.com>
2024-06-07 10:29:15 +0200

    auto login when token is available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import {AuthService} from "./services/auth.service";
 import {MenuService} from "./services/menu.service";
 import {NgIf} from "@angular/common";
 import {LogoutService} from "./services/logout.service";
+import {HttpClient} from "@angular/common/http";
+import {UserInterface} from "./models/user.interface";
+import {environment} from "../environments/environment";
+import {LoginService} from "./services/login.service";
 
 @Component({
   selector: 'dummy-root',
@@ -18,11 +22,17 @@ export class AppComponent implements OnInit {
   title = 'standalone-dummy-angular';
   menuService = inject(MenuService);
   authService = inject(AuthService);
+  loginService = inject(LoginService);
   logoutService = inject(LogoutService);
+  http = inject(HttpClient)
 
   ngOnInit(): void {
     console.log("Check called")
     this.menuService.setMenu()
+    this.http.get<{user: UserInterface}>(environment.apiUrl + "/user")
+      .subscribe((response) => {
+        this.loginService.login(response.user)
+      })
   }
 
   logout() {
